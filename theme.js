758d3adbed6c5f58383e4cab90c3668a70d81714
menuwarunkmilenial <dartoulhaq@gmail.com>
2026-05-08 20:47:52 +0700

    Update .4

--- theme.js
+++ theme.js
@@ -3,19 +3,19 @@
             theme: {
                 extend: {
                     fontFamily: {
-                        sans: ['Inter', 'sans-serif'], // Font mirip San Francisco
+                        sans: ['Inter', 'sans-serif'],
                     },
                     colors: {
                         apple: {
-                            bg: '#F5F5F7',       // Background khas Apple
-                            text: '#1D1D1F',     // Teks utama Apple
+                            bg: '#F5F5F7',       // Background
+                            text: '#1D1D1F',     // Teks utama 
                             muted: '#414147',    // Teks sekunder
-                            blue: '#85058a',     // Primary Button Apple
+                            blue: '#85058a',     // Primary Button 
                             blueHover: '#5ceb09' // Hover Button
                         }
                     },
                     boxShadow: {
-                        'ios': '0 4px 24px rgba(0, 0, 0, 0.04)', // Shadow super lembut
+                        'ios': '0 4px 24px rgba(0, 0, 0, 0.04)', // Shadow 
                     }
                 }
             }
